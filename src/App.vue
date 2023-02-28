155c9c99346df6ac2cd9330f7b43116ce78244d9
Laura Izaguirre <laurar.izaguirre@gmail.com>
2023-02-28 11:58:34 -0600

    Finish static components

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    data() {
+      return {
+        books: [
+          {
+            id: 1,
+            title: "Narnia",
+            author: "C.S Lewis",
+            releaseDate: "1999-12-01"
+          },
+          {
+            id: 2,
+            title: "Narnia",
+            author: "C.S Lewis",
+            releaseDate: "1999-12-01"
+          },
+          {
+            id: 3,
+            title: "Narnia",
+            author: "C.S Lewis",
+            releaseDate: "1999-12-01"
+          }
+        ],
+      }
+    },
+  };
+</script>
+
+<template>
+  <h1>My books</h1>
+  <ul>
+    <book-info></book-info>
+  </ul>
+</template>
+
+<style>
+  h1 {
+    color: aqua;
+  }
+</style>
