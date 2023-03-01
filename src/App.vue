2ec1d1f49f67491bfae798dd346d4ed707d2a834
Laura Izaguirre <laurar.izaguirre@gmail.com>
2023-02-28 19:32:26 -0600

    Finish dynamic components

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,27 @@
 <template>
   <h1>My books</h1>
   <ul>
-    <book-info></book-info>
+    <book-info id="1"
+    title="Narnia"
+    author="C.S Lewis"
+    release-date="1999-12-01"
+    is-liked="1"
+    >
+  </book-info>
+  <book-info id="2"
+    title="NarniaII"
+    author="C.S Lewis"
+    release-date="1989-12-01"
+    is-liked="0"
+    >
+  </book-info>
+  <book-info id="3"
+    title="NarniaIII"
+    author="C.S Lewis"
+    release-date="1979-12-01"
+    is-liked="1"
+    >
+    </book-info>
   </ul>
 </template>
 
